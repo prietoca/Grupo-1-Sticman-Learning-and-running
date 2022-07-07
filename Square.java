8e86daa8d5ff28b8236623ad8a4d9785ad7f0fdf
prietoca <107828125+prietoca@users.noreply.github.com>
2022-07-06 19:47:06 -0500

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,20 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+	// properties
+	protected int side;
+	
+	// constructors
+	public Square( int side) {
+		
+		super(side, side);
+		this.side = side;
+		
+		
+	}
+
+	
+	
+
+}
