e69852ff7d0fd384256ad1b9c5c81e26ee8df94a
prietoca <107828125+prietoca@users.noreply.github.com>
2022-07-06 19:43:41 -0500

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,24 @@
+package shapes;
+
+public class Shape implements Locatable {
+	
+	// properties
+	int x, y;
+	
+	abstract double getArea();
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setLocation( int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+	}
+
+}
